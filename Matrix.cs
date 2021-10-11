88a8037bc75071ffd768ca8b9bd8cd219c1935de
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-11 21:26:53 +0300

    Add project files.

---
+++ Matrix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    class Matrix : IEnumerable<double>
+    {
+        double[,] _coefs;
+
+        public Matrix(double[,] coefs)
+        {
+            _coefs = coefs;
+        }
+
+        public Matrix() : this(new[,] { { 0.0 } })
+        {
+        }
+
+        public double this[int i, int j]
+        {
+            get => _coefs[i, j];
+            set => _coefs[i, j] = value;
+        }
+
+        public IEnumerator<double> GetEnumerator()
+        {
+            for (int i = _coefs.GetLength(0) - 1; i > -1; --i)
+                for (int j = _coefs.GetLength(1) - 1; j > -1; --j)
+                    yield return _coefs[i, j];
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Matrix
+{
+    class Program
+    {
+        static void Main()
+        {
+            Matrix m = new Matrix(new double[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } });
+
+            foreach (double coef in m)
+            {
+                Console.WriteLine(coef);
+            }
+
+            Console.Read();
+        }
+    }
+}
